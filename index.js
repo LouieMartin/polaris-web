4fb5057b5fa06336348672a1e876e5f58ab9ccd5
LouieMartin <louie.decierdo@gmail.com>
2023-08-14 09:51:03 +0800

    Added Quiescence Search, Negamax, and Simple Piece Evaluation.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const pieceValues = {
+  k: 20000,
+  q: 900,
+  r: 500,
+  b: 330,
+  n: 320,
+  p: 100,
+};
+
 const game = new Chess();
 
 let board;
@@ -5,6 +14,107 @@ let board;
 const onDragStart = (_, piece) => {
   if (piece.search(/^b/) !== -1) return;
   if (game.game_over()) return;
+};
+
+const evaluate = () => {
+  const perspective = game.turn() === 'w' ? 1 : -1;
+  
+  let score = 0;
+
+  for (let square of game.SQUARES) {
+    const piece = game.get(square);
+
+    if (!piece) continue;
+    const color = piece.color === 'w' ? 1 : -1
+
+    score += pieceValues[piece.type] * color;
+  }
+
+  return perspective * score;
+};
+
+const quiesce = (alpha, beta) => {
+  let evaluation = evaluate();
+
+  if (evaluation >= beta) {
+    return beta;
+  }
+
+  alpha = Math.max(evaluation, alpha);
+  const captures = game.ugly_moves().filter(move => move.captured);
+
+  for (let capture of captures) {
+    game.ugly_move(capture);
+    const evaluation = -quiesce(-beta, -alpha)
+
+    game.undo(capture);
+    if (evaluation >= beta) {
+      return beta;
+    }
+
+    alpha = Math.max(evaluation, alpha);
+  }
+
+  return alpha;
+};
+
+const negamax = (depth, alpha, beta) => {
+  depth = Math.max(0, depth);
+ 
+  const moves = game.ugly_moves();
+
+  if (game.in_checkmate()) {
+    return Number.NEGATIVE_INFINITY;
+  }
+
+  if (moves.length === 0) {
+    return 0;
+  }
+
+  if (depth === 0) {
+    return quiesce(alpha, beta);
+  }
+
+  for (let move of moves) {
+    game.ugly_move(move);
+    const evaluation = -negamax(depth - 1, -beta, -alpha);
+
+    game.undo(move);
+    if (evaluation >= beta) {
+      return beta;
+    }
+
+    alpha = Math.max(evaluation, alpha);
+  }
+
+  return alpha;
+};
+
+const findBestMove = depth => { 
+  const moves = game.ugly_moves();
+
+  let bestEvaluation = Number.NEGATIVE_INFINITY;
+  let bestMove;
+
+  for (let move of moves) {
+    const prettyMove = game.ugly_move(move);
+    const evaluation = -negamax(depth - 1, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+
+    game.undo(move);
+    if (bestEvaluation <= evaluation) {
+      bestEvaluation = evaluation;
+      bestMove = move;
+    }
+  }
+
+  return bestMove;
+};
+
+const playBestMove = depth => {
+  const move = findBestMove(depth);
+
+  game.ugly_move(move);
+  board.position(game.fen());
 };
 
 const onDrop = (source, target) => {
@@ -16,8 +126,8 @@ const onDrop = (source, target) => {
 
   if (move === null) return 'snapback';
 
-  // TODO: Make Polaris and make move here.
-}
+  playBestMove(4);
+};
 
 const onSnapEnd = () => {
   board.position(game.fen())
@@ -33,3 +143,4 @@ const config = {
 };
 
 board = Chessboard('board', config);
+
